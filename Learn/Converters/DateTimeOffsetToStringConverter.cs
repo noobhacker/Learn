3297e80c899b6c649060b56784bca493db534e43
Song <Song@DESKTOP-Q3K488L>
2016-08-26 01:58:15 +0800

    added datetimeoffset converter

---
+++ Learn/Converters/DateTimeOffsetToStringConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Learn.Converters
+{
+    public class DateTimeOffsetToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return ((DateTimeOffset)value).ToLocalTime().ToString("dddd, MMM dd yyyy");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
